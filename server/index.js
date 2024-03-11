26eaf958ad667dea01d2b1c722219166070df440
Manish Gupta <69005837+manishgupta72@users.noreply.github.com>
2024-03-12 02:06:04 +0530

    final code

---
+++ server/index.js
@@ -0,0 +1,40 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const authRoute = require("./router/auth");
+const contactRoute = require("./router/contact");
+const connectToDB = require("./db");
+const errorMiddleware = require("./middlewares/error-middleware");
+const services = require("./controller/service-controller");
+const adminRoute = require("./router/admin-router");
+const path = require("path");
+var MongoClient = require("mongodb").MongoClient;
+
+
+
+app.use(cors({
+  origin:[],
+  methods:["POST","GET","PUT","PATCH","DELETE"],
+  credentials:true,
+}));
+app.get("/",(req,res)=>{
+  res.json("Hello manish");
+})
+app.use(express.json());
+app.use("/api/auth", authRoute);
+app.use("/api/form", contactRoute);
+app.use("/api/data", services);
+app.use("/api/admin", adminRoute);
+
+
+
+app.use(errorMiddleware);
+// step2 hosting
+const PORT=3001;
+
+connectToDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`server started at post ${PORT}`);
+  });
+});
